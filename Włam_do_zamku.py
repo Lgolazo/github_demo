b0f64a5ed40af057c30fa1689844780de3926c65
Lgolazo <Mastah1238@gmail.com>
2021-03-10 21:01:30 +0100

    Pierwszy etap gry.

---
+++ Włam_do_zamku.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 10 19:12:58 2021
+
+@author: Mastah
+"""
+
+# %%
+
+imie = (input("Jak masz na imię? "))
+# Nie potrafię zablokować wpisania INT
+#if imie.isdigit():
+    #print('Proszę podać imię: ')
+
+print("Czesc " + imie.title())
+print("Chciałbym Cię zaprosić do gry Włam się do zamku. \nPrzed Tobą pierwsze drzwi. \nMasz 3 możliwosci do wyboru.")
+print("1. Otwierasz drzwi wytrychem \n2. Próbujesz wyłamać drzwi łomem \n3. Kopiesz je z buta.")
+
+
+
