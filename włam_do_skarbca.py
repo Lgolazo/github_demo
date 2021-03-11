623cf97d2f2399da02b3861bd81be58464d8a930
Lgolazo <Mastah1238@gmail.com>
2021-03-11 22:02:59 +0100

    Skończona gra

---
+++ włam_do_skarbca.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 10 19:12:58 2021
+
+@author: Mastah
+"""
+
+# %%
+
+imie = input("Jak masz na imię? ")
+# Nie potrafię zablokować wpisania INT
+#if imie.isdigit():
+    #print('Proszę podać imię: ')
+
+
+print("Czesc " + imie.title())
+print("Chciałbym Cię zaprosić do gry Włam się do Skarbca. \nSterowanie w grze za pomocą klawiatury numerycznej")
+print("Przed Tobą pierwsze drzwi. \nMasz 3 możliwosci do wyboru: ")
+print("1. Otwierasz drzwi wytrychem \n2. Próbujesz wyłamać drzwi łomem \n3. Kopiesz je z buta.")
+
+# Pierwszy droga wybór pierwszej opcji
+
+wybor1 = input("Wpisz co wybierasz: ")
+
+if wybor1 == "1":
+    print("Po kilku minutach udaje Ci się otworzyć drzwi \nGratulacje!!! \nIdziesz dalej")
+    print("Po wejsciu masz dwie drogi wyboru: \n1. Drzwi po lewej stronie \n2. Drzwi po prawej stronie ")
+
+# Drugi poziom
+    drzwi = input("Które drzwi wybierasz? ")
+
+    if drzwi == "1":
+        print("Przekręcasz klamkę, okazuje się że drzwi są otwarte. \nTrafiłes do kuchnii, ze stołu kradniesz kawałek kiełbasy")
+        print("Wracasz z powrotem")
+        print("2. Drzwi po prawej stronie")
+        while True:
+            drzwi_lvl2 = input("Drzwi po prawej wcisnij klawisz '2': ")
+            if drzwi_lvl2 == "2":
+                break
+        print("Znalazłes się w wielkim holu, przed sobą widzisz drzwi do skarbca.")
+        print("Niestety żeby się tam dostać musisz złamać kod")
+        print("Żeby złamać kod musisz rozwiązać zadanie matematyczne: Ile to jest 2+2")
+        while True:
+            wejscie = input("Podaj poprawny wynik: ")
+            if wejscie == "4":
+                print("Uzyskałes dostęp. \nBrawo udało Ci się dostać do skarbca. \nKoniec Gry!!!")
+                break
+            print("Zły kod spróbuj jeszcze raz.")
+    if drzwi == "2": 
+        print("Znalazłes się w wielkim holu, przed sobą widzisz drzwi do skarbca.")
+        
+# Trzeci poziom zagadka matematyczna
+        print("Niestety żeby się tam dostać musisz złamać kod")
+        print("Żeby złamać kod musisz rozwiązać zadanie matematyczne: Ile to jest 2+2")
+        while True:
+            wejscie = input("Podaj poprawny wynik: ")
+            if wejscie == "4":
+                print("Uzyskałes dostęp. \nBrawo udało Ci się dostać do skarbca. \nKoniec Gry!!!")
+                break
+            print("Zły kod spróbuj jeszcze raz.")
+#  Druga droga wybór drugiej opcji
+if wybor1 == "2":
+    print("Udało Ci się wywarzyć drzwi łomem \nGratulacje!!! \nIdziesz dalej")
+    print("Po wejsciu masz dwie drogi wyboru: \n1. Drzwi po lewej stronie \n2. Drzwi po prawej stronie ")
+
+# Drugi poziom  
+    drzwi = input("Które drzwi wybierasz? ")
+
+    if drzwi == "1":
+        print("Przekręcasz klamkę, okazuje się że drzwi są otwarte. \nTrafiłes do kuchnii, ze stołu kradniesz kawałek kiełbasy")
+        print("Wracasz z powrotem")
+        print("2. Drzwi po prawej stronie")
+        while True:
+            drzwi_lvl2 = input("Drzwi po prawej wcisnij klawisz '2': ")
+            if drzwi_lvl2 == "2":
+                break
+        print("Znalazłes się w wielkim holu, przed sobą widzisz drzwi do skarbca.")
+        print("Niestety żeby się tam dostać musisz złamać kod")
+        print("Żeby złamać kod musisz rozwiązać zadanie matematyczne: Ile to jest 2+2")
+        while True:
+            wejscie = input("Podaj poprawny wynik: ")
+            if wejscie == "4":
+                print("Uzyskałes dostęp. \nBrawo udało Ci się dostać do skarbca. \nKoniec Gry!!!")
+                break
+            print("Zły kod spróbuj jeszcze raz.")
+    if drzwi == "2": 
+        print("Znalazłes się w wielkim holu, przed sobą widzisz drzwi do skarbca.")
+
+# Trzeci poziom zagadka matematyczna
+        print("Niestety żeby się tam dostać musisz złamać kod")
+        print("Żeby złamać kod musisz rozwiązać zadanie matematyczne: Ile to jest 2+2")
+        while True:
+            wejscie = input("Podaj poprawny wynik: ")
+            if wejscie == "4":
+                print("Uzyskałes dostęp. \nBrawo udało Ci się dostać do skarbca.")
+                break
+            print("Zły kod spróbuj jeszcze raz.")
+# Trzecia droga   
+if wybor1 == "3":
+    print("Drzwi okazały się za mocne. \nZłamałes nogę \nNiestety na tym Twoja przygoda się kończy.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
